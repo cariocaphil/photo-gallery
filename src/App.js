73a2502d80e564fa714bb5490a84a80334433c73
cariocaphil <cariocaphil@gmail.com>
2023-03-08 23:53:41 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Image Gallery</h1>
+
+      <form>
+        <input type="text" placeholder="Search..." />
+        <button>Search</button>
+      </form>
+
+      <div className="image-grid">
+        {[...Array(100)].map((_, index) => (
+          <div className="image" key={index}>
+            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
